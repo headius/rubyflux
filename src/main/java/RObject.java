b2e8f2bc508a1b3044f0ad3128a12f1b701ede80
Charles Oliver Nutter <headius@headius.com>
2012-12-29 08:18:31 -0600

    Remove methods from RObject.java, since it's just a stub.

--- src/main/java/RObject.java
+++ src/main/java/RObject.java
@@ -1,35 +1,5 @@
 
-public class RObject
-    extends RKernel
-{
-
-
-    public RObject initialize() {
-        throw new RuntimeException("initialize");
-    }
-
-    public RObject hello_world() {
-        throw new RuntimeException("hello_world");
-    }
-
-    public RObject puts(RObject arg0) {
-        throw new RuntimeException("puts");
-    }
-
-    public RObject _plus_(RObject arg0) {
-        throw new RuntimeException("_plus_");
-    }
-
-    public RObject fib(RObject arg0) {
-        throw new RuntimeException("fib");
-    }
-
-    public RObject _lt_(RObject arg0) {
-        throw new RuntimeException("_lt_");
-    }
-
-    public RObject _minus_(RObject arg0) {
-        throw new RuntimeException("_minus_");
-    }
-
+public class RObject extends RKernel {
+    /* This class is just provided as a stub to allow subclasses to compile.
+       In a real run, it is generated anew based on input scripts. */
 }
